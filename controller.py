adbe7f6e6c0eed748ee0f1c5f48585f67a8333bf
Dayebga Babila Mutia II <roybmutia@gmail.com>
2021-06-29 10:47:08 +0100

    - Added api endpoints - Added controller

---
+++ controller.py
@@ -0,0 +1,44 @@
+import recommenders
+
+class Controller():
+    def __init__(self) -> None:
+        pass
+
+    def register_click(impression_id):
+        pass
+
+    def get_recommendations(user_id):
+        pass
+
+    def get_similar_items(item_id):
+        pass
+
+    def get_associated_items(item_id):
+        pass
+
+    def add_user(user):
+        pass
+
+    def modify_user(user_id, user):
+        pass
+
+    def delete_user(user_id):
+        pass
+
+    def add_service(service):
+        pass
+
+    def modify_service(service_id, service):
+        pass
+
+    def delete_service(service_id):
+        pass
+
+    def add_subscription(subscription):
+        pass
+
+    def modify_subscription(subscription_id, subscription):
+        pass
+
+    def delete_subscription(subscription_id):
+        pass
