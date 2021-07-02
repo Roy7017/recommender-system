12048d4529cd6da319dbdd749a733f82efed6c00
Dayebga Babila Mutia II <roybmutia@gmail.com>
2021-07-02 17:16:26 +0100

    - Refactored database fields

--- models/history_models.py
+++ models/history_models.py
@@ -2,21 +2,21 @@ from models.interaction_models import User
 from database import db
 
 class RecommendationHistory(db.Document):
-    id = db.StringField(primary_key=True)
-    duration = db.StringField()
-    date = db.StringField()
+    # id = db.StringField(primary_key=True)
+    duration = db.DecimalField() # in seconds
+    date = db.DateTimeField() # in iso format
 
 class Recommendations(db.Document):
-    id = db.StringField(primary_key=True)
-    items = db.StringField()
+    # id = db.StringField(primary_key=True)
+    items = db.ListField(db.StringField())
 
     user = db.LazyReferenceField(User)
     recommendation_history = db.LazyReferenceField(RecommendationHistory)
 
 class TrainingHistory(db.Document):
-    id = db.StringField(primary_key=True)
-    duration = db.StringField()
-    date = db.StringField()
+    # id = db.StringField(primary_key=True)
+    duration = db.DecimalField() # in seconds
+    date = db.DateTimeField()
 
     global_mpr = db.DecimalField()
     global_recall = db.DecimalField()
